c80062e75086b009d9a93b141d9978ca8a22e9ee
disamiron <denis.mironov@ddgcorp.ru>
2022-07-31 21:26:28 +0300

    add mat-table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { StorageService } from './services/storage/storage.service';
 import { UsersService } from './services/users/users.service';
+import { User } from './services/users/users.service.type';
 import { StorageType } from './services/storage/storage.type';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { MatTableDataSource } from '@angular/material/table';
 
 @UntilDestroy()
 @Component({
@@ -11,21 +13,31 @@ import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
+  @ViewChild('carriersContainer') public carriersContainer!: ElementRef;
   title = 'TableFormOfDataEdit';
+
+  public dataSource: MatTableDataSource<User[]> = new MatTableDataSource();
+
+  public columnsToDisplay = ['users', 'mail', 'phone'];
+
   constructor(
     private _userService: UsersService,
     private _storageService: StorageService
   ) {}
 
   public ngOnInit(): void {
-    console.log(this._storageService.getItem(StorageType.Users));
     if (!this._storageService.getItem(StorageType.Users)) {
       this._userService
         .getUsers()
         .pipe(untilDestroyed(this))
         .subscribe((v) => {
           this._storageService.setItem(StorageType.Users, v.users);
+          this.dataSource.data = this._storageService.getItem(
+            StorageType.Users
+          );
         });
+    } else {
+      this.dataSource.data = this._storageService.getItem(StorageType.Users);
     }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, MatTableModule],
+  exports: [MatTableModule],
   providers: [],
   bootstrap: [AppComponent],
 })
